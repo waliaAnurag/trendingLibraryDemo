cc5d2c6441016f0a33a8d3486b221a0752983ad8
waliaAnurag <walianura5@gmail.com>
2024-03-18 18:30:24 +0530

    minro changes omplemented

--- src/middleware.js
+++ src/middleware.js
@@ -8,10 +8,10 @@ export async function middleware(request) {
   const { pathname } = request.nextUrl
 
 
-  if (pathname.includes("dashboard") && !(request.cookies.get('next-auth.session-token')?.value)) {
+  if (pathname.includes("dashboard") && !(request.cookies.get('__Secure-next-auth.session-token')?.value)) {
     return NextResponse.redirect(new URL('/', request.url));
   }
-  if (((pathname === "/") || (pathname === "/auth/signup")) && (request.cookies.get('next-auth.session-token')?.value)) {
+  if (((pathname === "/") || (pathname === "/auth/signup")) && (request.cookies.get('__Secure-next-auth.session-token')?.value)) {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
