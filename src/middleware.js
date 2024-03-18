a98f85599646fdefdc0ca2ef663232b6554d4113
waliaAnurag <walianura5@gmail.com>
2024-03-18 18:23:19 +0530

    dashboard protected

--- src/middleware.js
+++ src/middleware.js
@@ -8,9 +8,9 @@ export async function middleware(request) {
   const { pathname } = request.nextUrl
 
 
-  // if (pathname.includes("dashboard") && !(request.cookies.get('next-auth.session-token')?.value)) {
-  //   return NextResponse.redirect(new URL('/', request.url));
-  // }
+  if (pathname.includes("dashboard") && !(request.cookies.get('next-auth.session-token')?.value)) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
   if (((pathname === "/") || (pathname === "/auth/signup")) && (request.cookies.get('next-auth.session-token')?.value)) {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
